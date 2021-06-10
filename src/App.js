60fdabe8151296c3895c83c4dc7aaac252b9b3da
christopherfelt <chris.h.felt@gmail.com>
2021-06-09 20:26:16 -0600

    added sign up and email confirmation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import Amplify from "aws-amplify";
 import awsconfig from "./aws-exports";
 // import { AmplifySignOut, withAuthenticator } from "@aws-amplify/ui-react";
+import { Auth } from "aws-amplify";
 import "bootstrap/dist/css/bootstrap.css";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
@@ -20,17 +21,53 @@ function App() {
   const [signUpEmail, setSignUpEmail] = useState("");
   const [signUpPassword, setSignUpPassword] = useState("");
 
+  const [showCodeForm, setShowCodeForm] = useState(false);
+  const [confirmCode, setConfirmCode] = useState("");
+
   const [signInEmail, setSignInEmail] = useState("");
   const [signInPassword, setSignInPassword] = useState("");
 
-  const signUpHandler = (e) => {
+  const [showLoggedInButton, setShowLoggedInButton] = useState(false);
+
+  const signUpHandler = async (e) => {
     e.preventDefault();
-    console.log("Sign Up Stuff", signUpEmail, signUpPassword);
+    try {
+      const { user } = await Auth.signUp({
+        username: signUpEmail,
+        password: signUpPassword,
+      });
+      console.log("Sign Up Success");
+      console.log(user);
+    } catch (error) {
+      console.log("error signing up:", error);
+    }
+    console.log("Hey from sign up page");
+    setShowCodeForm(true);
+  };
+
+  const confirmCodeHandler = async (e) => {
+    e.preventDefault();
+    try {
+      await Auth.confirmSignUp(signUpEmail, confirmCode);
+      console.log("confirmation works");
+    } catch (error) {
+      console.log("error confirming sign up:", error);
+    }
   };
 
   const signInHandler = (e) => {
     e.preventDefault();
+    setShowLoggedInButton(true);
     console.log("Sign In Stuff", signInEmail, signInPassword);
+  };
+
+  const authenticateUserHandler = (e) => {
+    // try {
+
+    // } catch (error) {
+    //   console.log("User is not signed in:", error)
+    // }
+    console.log("Hi from authenticate user button");
   };
 
   const signOutHandler = (e) => {
@@ -49,48 +86,71 @@ function App() {
               className="border d-flex align-items-center justify-content-center"
               xs={4}
             >
-              <Form onSubmit={signUpHandler}>
-                <h2>Sign Up</h2>
-                <Form.Group>
-                  <Form.Control
-                    type="email"
-                    placeholder="email"
-                    onChange={(e) => setSignUpEmail(e.target.value)}
-                  />
-                  <Form.Control
-                    type="password"
-                    placeholder="password"
-                    onChange={(e) => setSignUpPassword(e.target.value)}
-                  />
-                  <Button variant="primary" type="submit">
-                    Sign Up
-                  </Button>
-                </Form.Group>
-              </Form>
+              {" "}
+              {!showCodeForm ? (
+                <Form onSubmit={signUpHandler}>
+                  <h2>Sign Up</h2>
+                  <Form.Group>
+                    <Form.Control
+                      type="email"
+                      placeholder="email"
+                      onChange={(e) => setSignUpEmail(e.target.value)}
+                    />
+                    <Form.Control
+                      type="password"
+                      placeholder="password"
+                      onChange={(e) => setSignUpPassword(e.target.value)}
+                    />
+                    <Button variant="primary" type="submit">
+                      Sign Up
+                    </Button>
+                  </Form.Group>
+                </Form>
+              ) : (
+                <Form onSubmit={confirmCodeHandler}>
+                  <h2>Confirm Email</h2>
+                  <Form.Group>
+                    <Form.Control
+                      type="text"
+                      placeholder="Confirmation Code"
+                      onChange={(e) => setConfirmCode(e.target.value)}
+                    />
+                    <Button variant="primary" type="submit">
+                      Confirm
+                    </Button>
+                  </Form.Group>
+                </Form>
+              )}
             </Col>
             <Col
               style={colDismensions}
               className="border d-flex align-items-center justify-content-center"
               xs={4}
             >
-              <Form onSubmit={signInHandler}>
-                <h2>Sign In</h2>
-                <Form.Group>
-                  <Form.Control
-                    type="email"
-                    placeholder="email"
-                    onChange={(e) => setSignInEmail(e.target.value)}
-                  />
-                  <Form.Control
-                    type="password"
-                    placeholder="password"
-                    onChange={(e) => setSignInPassword(e.target.value)}
-                  />
-                  <Button variant="primary" type="submit">
-                    Sign In
-                  </Button>
-                </Form.Group>
-              </Form>
+              {!showLoggedInButton ? (
+                <Form onSubmit={signInHandler}>
+                  <h2>Sign In</h2>
+                  <Form.Group>
+                    <Form.Control
+                      type="email"
+                      placeholder="email"
+                      onChange={(e) => setSignInEmail(e.target.value)}
+                    />
+                    <Form.Control
+                      type="password"
+                      placeholder="password"
+                      onChange={(e) => setSignInPassword(e.target.value)}
+                    />
+                    <Button variant="primary" type="submit">
+                      Sign In
+                    </Button>
+                  </Form.Group>
+                </Form>
+              ) : (
+                <Button variant="primary" onClick={authenticateUserHandler}>
+                  Is user logged in?
+                </Button>
+              )}
             </Col>
             <Col
               style={colDismensions}
